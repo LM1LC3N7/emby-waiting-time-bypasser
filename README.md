767883870ff7919c74c050a2208323a81900fd56
Louis MILCENT <louis@lmilcent.com>
2017-10-23 19:49:28 +0200

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# emby-waiting-time-bypasser
-Getting rid of the popup "Discover Emby Premiere"
+# Emby Waiting Time ByPasser
+
+![Emby Premiere Popup](https://i.imgur.com/26lkkit.png)
+
+## French
+Se débarraser du popup "Découverte de Emby Premiere" !
+
+Le but de ce *userscript* est d'automatiser la suppression du popup pour passer à la vidéo suivante.
+
+**Installation**
+1. Installer [tampermonkey](https://tampermonkey.net/) en extension au navigateur web
+2. Ajouter ce script à tampermonkey (copier / coller du code)
+3. Changer la ligne `// @match        http*://media.lmilcent.com/*` du script avec l'URL voulue
+
+
+## English
+Getting rid of the popup "Discover Emby Premiere" !
+
+The purpose of this userscript is to automate the removal of the popup to move on to the next video.
+
+**Installation***
+1. Install[tampermonkey](https://tampermonkey.net/) as an extension to the web browser
+2. Add this script to tampermonkey (copy/paste code)
+3. Change the line `// @match        http*://media.lmilcent.com/*` of the script with the desired URL
+
+The purpose of this userscript is to automate the removal of the popup to move on to the next video.
---
+++ waitingTimeByPasser.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Emby waiting time bypasser
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Getting rid of the popup "Discover Emby Premiere"
+// @author       Louis MILCENT
+// @match        http*://media.lmilcent.com/*
+// @downloadURL  https://github.com/flexbrane/emby-waiting-time-bypasser/releases
+// @supportURL   https://github.com/flexbrane/emby-waiting-time-bypasser/issues
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  let waitBtn = '.formDialogContent .dialogContentInner .formDialogFooter .continueTimeText';
+  let continueBtn = '.formDialogContent .dialogContentInner .formDialogFooter .btnContinue';
+
+  // Time in ms
+  let checkEvery = 250;
+
+  setInterval(function () {
+    if (document.querySelectorAll(waitBtn).length) {
+      console.log('Deleting the waiting button!');
+      removeWaiting();
+    }
+  }, checkEvery);
+
+  //
+  // Function called when the button is detected
+  //  - Hide the waiting time
+  //  - Show the continue button
+  //  - Click on it
+  //
+  function removeWaiting () {
+    console.log('==================================');
+    console.log('');
+    console.log('');
+    console.log('   EMBY WAITING TIME BYPASSED!!   ');
+    console.log('');
+    console.log('');
+    console.log('==================================');
+    document.querySelectorAll(waitBtn)[0].classList.add('hide');
+    document.querySelectorAll(continueBtn)[0].classList.remove('hide');
+    document.querySelectorAll(continueBtn)[0].click();
+  }
+})();
