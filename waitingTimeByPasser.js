9c8bd7346fe7562577c59333c28252959e51d53a
Louis MILCENT <louis@lmilcent.com>
2017-12-05 16:34:17 +0100

    version 0.4

--- waitingTimeByPasser.js
+++ waitingTimeByPasser.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Emby waiting time bypasser
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Getting rid of the popup "Discover Emby Premiere"
 // @author       Louis MILCENT
 // @match        http*://media.lmilcent.com/*
